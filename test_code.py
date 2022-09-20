c60ee5a21d9b1f319cc5610a601d5903dd2de6e1
mixej <mixeev.v@gmail.com>
2022-09-20 23:30:29 +0300

    v0.0.1c

--- test_code.py
+++ test_code.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+import os
+import subprocess
 
-print("hello")
+
+
+FILENAME = 'update_list.txt'
+
+#p = input("hello: ")
+
+last_ver = subprocess.run(["git", "log", "--pretty=format:'%s'", "-1"], stdout=subprocess.PIPE, text=True, encoding='utf-8' )
+#version = last_ver
+
+with open(FILENAME,'a+') as file:
+	source = file.readlines()
+	b = []  # получаешь свои результаты
+	s = ''.join(b) + '\n'
+	source.insert(0, s)
+	file.writelines(source)
+	file.write(last_ver.stdout)
+	
+	
+print("successful")	
