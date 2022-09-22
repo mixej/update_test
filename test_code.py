c759d59e1f1aa36c14d179e5a2ae4c834b973194
mixej <mixeev.v@gmail.com>
2022-09-22 22:01:37 +0300

    v0.0.1c1

--- test_code.py
+++ test_code.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
+# получение номера обновления
 import os
 import subprocess
 
-
-
 FILENAME = 'update_list.txt'
 
-#p = input("hello: ")
+def get_update_num()
+	last_ver = subprocess.run(["git", "log", "--pretty=format:'%s'", "-1"], stdout=subprocess.PIPE, text=True, encoding='utf-8' )
 
-last_ver = subprocess.run(["git", "log", "--pretty=format:'%s'", "-1"], stdout=subprocess.PIPE, text=True, encoding='utf-8' )
-#version = last_ver
+	with open(FILENAME,'w') as file:
+		file.write(last_ver.stdout + '\n')
+	
 
-with open(FILENAME,'a+') as file:
-	source = file.readlines()
-	b = []  # получаешь свои результаты
-	s = ''.join(b) + '\n'
-	source.insert(0, s)
-	file.writelines(source)
-	file.write(last_ver.stdout)
-	
-	
-print("successful")	
+#print("successful")	
---
+++ test_update.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#сравнение текущего номера версии на гитхабе с последним сохраненным в фаил
+import os
+import subprocess
+import update_num
+
+FILENAME = 'update_list.txt'
+
+
+def get_new_ver():
+	new_ver = subprocess.run(["git", "log", "--pretty=format:%s", "-1"], stdout=subprocess.PIPE, text=True, encoding='utf-8' )
+	return str(new_ver.stdout)
+	
+def get_old_ver():
+	with open(FILENAME,'r') as file:
+		for line in file:
+			old_ver = line
+			return str(old_ver)	
+
+def update_file_version():
+	with open(FILENAME,'w') as file:
+		file.write(get_new_ver())		
+
+def update_version(): 	
+	old_version = get_old_ver()
+	new_version = get_new_ver()	
+	if new_version == old_version:
+		print("no update")
+	else:
+		print("can update to: ", new_version)
+		print(old_version)
+		print(new_version)
+		
+if __name__ == '__main__':
+	
+	update_version()
+	update_file_version()
